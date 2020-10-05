39c9f856cae9e121692f9bb994186d2568a24087
akira.iguchi <akira.iguchi@techacademy.jp>
2020-10-05 13:36:49 +0000

    osakaturiba

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,17 +7,17 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_public = false
     config.fog_credentials = {
      
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],   
       :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+       # path_style: true
     }
     config.fog_directory     =  ENV['S3_BUCKET']
     
-    config.cache_storage = :fog
-   
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
  
