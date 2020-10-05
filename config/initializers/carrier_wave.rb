f63fcd7d498e559395dbaa073cc682ef64e8ee3b
akira.iguchi <akira.iguchi@techacademy.jp>
2020-10-05 08:01:15 +0000

    osakaturiba

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,8 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
@@ -5,7 +10,7 @@ if Rails.env.production?
      
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'],   
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
