8ca5ffd118097ada3d6ad4a7d2220b395b0ea5d0
akira-iguchi <iguchiakira@akira.local>
2020-12-16 17:40:20 +0900

    “osakaturiba”

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,7 @@ set :repo_url, "git@github.com:akira-iguchi/osakaturiba.git"
 set :deploy_to, "/var/www/rails/osakaturiba"
 
 # シンボリックリンクをはるファイル。
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_files, fetch(:linked_files, []).push('config/credentials/production.key')
 
 # シンボリックリンクをはるフォルダ。
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
