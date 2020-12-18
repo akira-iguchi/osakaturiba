e20abb3c8b1c967792a7505d59ff90228b29afcc
akira-iguchi <iguchiakira@akira.local>
2020-12-18 21:06:05 +0900

    “osakaturiba”

--- config/deploy.rb
+++ config/deploy.rb
@@ -84,6 +84,6 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+  S3_ACCESS_KEY_ID: ENV["S3_ACCESS_KEY_ID"],
+  S3_SECRET_KEY: ENV["S3_SECRET_KEY"]
 }
