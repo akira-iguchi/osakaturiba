3b05462b0fd57875bcafba52674b6f84d587b0eb
akira-iguchi <iguchiakira@akira.local>
2020-12-22 00:21:10 +0900

    “osakaturiba”

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,6 @@ set :deploy_to, "/var/www/rails/osakaturiba"
 # シンボリックリンクをはるファイル。
 set :linked_files, fetch(:linked_files, []).push('config/credentials/production.key')
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
-set :linked_files, fetch(:linked_files, []).push('.env')
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
