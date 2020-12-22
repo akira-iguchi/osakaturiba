f309fb5c24bddcec05d03abc6723c6920c59a4b0
akira-iguchi <iguchiakira@akira.local>
2020-12-22 22:09:27 +0900

    “osakaturiba”

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :linked_files, fetch(:linked_files, []).push('config/credentials/production.
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 set :rbenv_type, :user
 
